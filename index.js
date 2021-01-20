57c96b299e8720b289a17fb2bcd03c5bb3e67670
Nathan Castaldi <nathan@castaldi.dev>
2021-01-20 18:25:33 -0500

    abstracted out all menu functions to menuSystem.js; confirmed working

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const clear = require("clear");
 const figlet = require("figlet");
 
 // Include local classes
-
+const menuSystem = require("./lib/menuSystem.js");
 
 // Create connection to database
 const connection = mysql.createConnection({
@@ -57,119 +57,5 @@ function init() {
     // });
 
     // Call main menu function
-    mainMenu();
-}
-
-function mainMenu() {
-    // Display top-level app menu
-    inquirer.prompt(
-        [
-            {
-                type: "list",
-                message: "MAIN MENU | Make a Selection:",
-                choices: ["Add", "View", "Update Roles", "EXTRA: Delete", "EXTRA: View Department Budget Utilization", "Exit"],
-                name: "menuAction"
-            }
-        ]
-    ).then(({ menuAction }) => {
-        switch (menuAction) {
-            case "Add":
-                addSubMenu();
-                break;
-            case "View":
-                viewSubMenu();
-                break;
-            case "Update Roles":
-                // updateSubMenu();
-                break;
-            case "EXTRA: Delete":
-                // deleteSubMenu();
-                break;
-            case "EXTRA: View Department Budget Utilization":
-                // displayBudgets();
-                break;
-            default:
-                connection.end();
-        }
-    });
-}
-
-function addSubMenu() {
-    // Clear screen before prompting user
-    clear();
-
-    // Display sub-level Add menu
-    inquirer.prompt(
-        [
-            {
-                type: "list",
-                message: "ADD | Make a Selection:",
-                choices: ["Department", "Employee", "Role", "Go to Main Menu"],
-                name: "menuAction"
-            }
-        ]
-    ).then(({ menuAction }) => {
-        switch (menuAction) {
-            case "Department":
-                // create department
-                break;
-            case "Employee":
-                // create employee
-                break;
-            case "Role":
-                // create role
-                break;
-            default:
-                // Clear screen before prompting user
-                clear();
-
-                // Return user to main menu
-                mainMenu();
-        }
-    });
-
-}
-
-function viewSubMenu() {
-    // Clear screen before prompting user
-    clear();
-
-    // Display sub-level Add menu
-    inquirer.prompt(
-        [
-            {
-                type: "list",
-                message: "VIEW | Make a Selection:",
-                choices: ["Departments", "Employees by Last Name", "EXTRA: Employees By Manager", "Roles", "Go to Main Menu"],
-                name: "menuAction"
-            }
-        ]
-    ).then(({ menuAction }) => {
-        switch (menuAction) {
-            case "Departments":
-                // view departments
-                break;
-            case "Employees By Last Name":
-                // view employees by last name
-                break;
-            case "EXTRA: Employees By Manager":
-                // view employees by manager
-                break;
-            case "Roles":
-                // view roles
-                break;
-            default:
-                // Clear screen before prompting user
-                clear();
-
-                // Return user to main menu
-                mainMenu();
-        }
-    });
-
-}
-
-
-function exit() {
-    connection.end();
+    menuSystem.mainMenu();
 }
