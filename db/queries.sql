c79c509c12424ad4db3bfc773524959b62603a6f
Nathan Castaldi <nathan@castaldi.dev>
2021-01-21 10:07:05 -0500

    updated queries.sql

--- db/queries.sql
+++ db/queries.sql
@@ -1,5 +1,9 @@
 USE employeesDB;
 
+SELECT * FROM employees;
+SELECT * FROM roles;
+SELECT * FROM departments;
+
 -- View Employee By Last Name
 SELECT last_name, first_name, role_id, manager_id FROM employees
 ORDER BY last_name;
@@ -15,3 +19,14 @@ ORDER BY name;
 -- View Roles
 SELECT title FROM roles
 ORDER BY title;
+
+-- List of managers
+SELECT id, first_name, last_name FROM employees
+WHERE manager_id != 'NULL'
+ORDER BY last_name; 
+
+-- List of managers and roles
+SELECT employees.id, employees.first_name, employees.last_name, employees.role_id, employees.manager_id, roles.id, roles.title
+FROM employees, roles
+WHERE manager_id != 'NULL'
+ORDER BY last_name; 
